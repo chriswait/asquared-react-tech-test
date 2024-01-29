1a2a82a14324e68b6f59eac669a38e17c86bf791
Chris Wait <2563474+chriswait@users.noreply.github.com>
2024-01-29 19:35:49 +0000

    adds react-query, fetch some beers!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-function App() {
-  return (
+import { useQuery } from 'react-query'
+
+const useBeers = () =>
+  useQuery({
+    queryKey: ['beers'],
+    queryFn: async () => {
+      const response = await fetch('https://api.punkapi.com/v2/beers')
+      return await response.json()
+    },
+  })
+
+const App = () => {
+  const { data: beers, isLoading, isError } = useBeers()
+  return isError ? (
+    <div>Oops! Something went wrong</div>
+  ) : isLoading ? (
+    <div>Loading</div>
+  ) : (
     <>
-      <h1>Hello, World!</h1>
+      <h1>Beers!</h1>
+      <ul>
+        {beers.map(({ id, name }) => (
+          <li key={`beer-${id}`}>{name}</li>
+        ))}
+      </ul>
     </>
   )
 }
