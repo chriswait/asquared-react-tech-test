288a8b4f3d2b8b83e5874bc97be2de1b15136840
Chris Wait <2563474+chriswait@users.noreply.github.com>
2024-01-30 22:13:04 +0000

    improve API error handling
    https://tanstack.com/query/latest/docs/framework/react/guides/query-functions?from=reactQueryV3#usage-with-fetch-and-other-clients-that-do-not-throw-by-default

--- src/queries.ts
+++ src/queries.ts
@@ -6,6 +6,10 @@ export const useBeers = () =>
     queryKey: ['beers'],
     queryFn: async () => {
       const response = await fetch('https://api.punkapi.com/v2/beers')
+      if (!response.ok) {
+        const errorResponse = await response.json()
+        throw new Error(errorResponse.message)
+      }
       return (await response.json()) as Beer[]
     },
   })
@@ -15,6 +19,10 @@ export const useBeer = (id: number) =>
     queryKey: [`beer-${id}`],
     queryFn: async () => {
       const response = await fetch(`https://api.punkapi.com/v2/beers/${id}`)
+      if (!response.ok) {
+        const errorResponse = await response.json()
+        throw new Error(errorResponse.message)
+      }
       return (await response.json()) as Beer[]
     },
   })
