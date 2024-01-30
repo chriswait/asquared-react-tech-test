f62d84c47e2277697dd29b95933c677968a15527
Chris Wait <2563474+chriswait@users.noreply.github.com>
2024-01-30 00:40:10 +0000

    adds fuse for fuzzy searching on name/description

--- src/screens/BeerListScreen.jsx
+++ src/screens/BeerListScreen.jsx
@@ -1,27 +1,54 @@
 import { useNavigate } from 'react-router-dom'
 import { BeerListItem } from '../components/BeerListItem'
 import { useBeers } from '../queries/useBeers'
+import { useMemo, useState } from 'react'
+import Fuse from 'fuse.js'
 
 export const BeerListScreen = () => {
-  const { data: beers, isLoading, isError } = useBeers()
   const navigate = useNavigate()
-  return isError ? (
-    <div>Oops! Something went wrong</div>
-  ) : isLoading ? (
-    <div>Loading</div>
-  ) : (
+  const { data: beers, isLoading, isError } = useBeers()
+  // Instead of querying the API with parameters, we'll do a local fuzzy search using fuse
+  const [search, setSearch] = useState('')
+  // Avoid reindexing fuse unless beers changes
+  const fuse = useMemo(
+    () =>
+      new Fuse(beers ?? [], {
+        keys: ['name', 'description'], // Allow searching over title and description
+        threshold: 0.2,
+      }),
+    [beers]
+  )
+  // Avoid repeated searches unless the fuse instance, beers, or search have changed
+  const searchedBeers = useMemo(
+    () => (search.length ? fuse.search(search).map(({ item }) => item) : beers),
+    [fuse, beers, search]
+  )
+  return (
     <>
-      <ul>
-        {beers
-          .slice(0, 10) // You should display only 10 drinks from the API.
-          .map((beer) => (
-            <BeerListItem
-              key={`beer-${beer.id}`}
-              beer={beer}
-              onClick={() => navigate(`/beer/${beer.id}`)}
-            />
-          ))}
-      </ul>
+      <input
+        placeholder="Search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      {isError ? (
+        <div>Oops! Something went wrong</div>
+      ) : isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <>
+          <ul>
+            {searchedBeers
+              .slice(0, 10) // You should display only 10 drinks from the API.
+              .map((beer) => (
+                <BeerListItem
+                  key={`beer-${beer.id}`}
+                  beer={beer}
+                  onClick={() => navigate(`/beer/${beer.id}`)}
+                />
+              ))}
+          </ul>
+        </>
+      )}
     </>
   )
 }
